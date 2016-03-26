cd00eda9db4e0d33afefb1349f679f87dc22b298
defelice <defelice@MBDC-MBP-DEFELICE.local>
2016-03-26 16:00:06 -0400

    some boilerplate cat code

---
+++ cat.js
@@ -0,0 +1,36 @@
+﻿#pragma strict
+
+function Start() {
+
+	stateEnum = {
+		HOLD:0,
+		SETTLE:1,
+		UNSETTLE:2
+	}
+
+	function Cat() {
+		var state = stateEnum.SETTLE;
+		this.getHappiness = function() {};
+		this.updateHappiness = function() {
+			this.tempHappiness = gameObject.GetComponent<zone>().temperature; * tempOpinion;
+		};
+		this.settle = function() {};
+	}
+}
+
+function Update() {
+
+}
+
+function OnTriggerEnter( col : collider ) {
+	console.log(col.toString);
+	/*
+	switch ( col.toString ) {
+		case "hot":
+			updateHot;
+			break;
+		case "wet":
+			updateWet:
+	}
+	*/
+}
