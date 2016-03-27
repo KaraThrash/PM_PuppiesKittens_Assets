c103e2fe6eba1ee9aa06dd786e8327c8ae0f541c
Kara Thrash <dvdonato@gmail.com>
2016-03-27 13:09:41 -0400

    undoing my nonsense

--- _scripts/Enviroment.cs
+++ _scripts/Enviroment.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class Enviroment : MonoBehaviour {
     public bool held;
+    public float clickSpot;
 	// Use this for initialization
 	void Start () {
 	
@@ -11,33 +12,35 @@ public class Enviroment : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-        if (held == true)
-        {
+        //if (held == true)
+        //{
             //transform.position = Vector3.MoveTowards(transform.position, Input.mousePosition, Time.deltaTime);
             //transform.LookAt(Input.mousePosition);
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-            if (Physics.Raycast(ray, out hit))
-            {
+            //Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            //RaycastHit hit;
+            //if (Physics.Raycast(ray, out hit))
+            //{
                 // transform.LookAt(new Vector3(hit.point.x, transform.position.y, hit.point.z));
-                transform.position = Vector3.MoveTowards(transform.position, new Vector3(hit.point.x,transform.position.y,hit.point.z), Time.deltaTime*0.3f);
+               // transform.position = Vector3.MoveTowards(transform.position, new Vector3(hit.point.x,transform.position.y,hit.point.z), Time.deltaTime*0.3f);
                 //Vector3 targetDir = new Vector3(hit.point.x, transform.position.y, hit.point.z) - transform.position;
                 //float step = 1 * Time.deltaTime;
                 //Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, -step, 0.0F);
 
                 //transform.rotation = Quaternion.LookRotation(newDir);
 
-            }
-        }
-        if (Input.GetMouseButtonUp(0))
-        {
-            held = false;
-        }
+            //}
+        //}
+        //if (Input.GetMouseButtonUp(0))
+        //{
+        //    held = false;
+        //}
     }
-    public void OnMouseDown()
+    public void OnMouseDrag()
     {
-        if (held == false)
-        { held = true; }
-        else { held = false; }
+
+
+        //if (held == false)
+        //{ held = true; }
+        //else { held = false; }
     }
 }
