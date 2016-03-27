41aeeb89f8d4cc8753f7dc09c4a770b51b0a4470
defelice <defelice@MBDC-MBP-DEFELICE.local>
2016-03-27 11:43:40 -0400

    javascript, finally

---
+++ Plugins/Zone.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Zone : MonoBehaviour {
+    public int hot;
+    public int wet;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
