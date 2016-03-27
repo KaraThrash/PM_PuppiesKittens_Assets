8a685e1ad9484204e7cd3aafbbfccf1df4ed8895
defelice <defelice@MBDC-MBP-DEFELICE.local>
2016-03-27 12:08:06 -0400

    i keep thinking this is svn

--- _scripts/Cat.js
+++ _scripts/Cat.js
@@ -1,10 +1,65 @@
 ﻿#pragma strict
 
-// when object instantiates
-function Start () {
+// i should find a unityscript linter
 
+public class Cat
+{
+
+	public var happiness : int;
+	public var envScore : int;
+	public var likes : Object;
+
+	public function Cat( h : int, e : int, l : Object ) {
+		happiness = h; // 0 to 100
+		envScore  = e; // 0 to 100, rate of change of happiness
+		likes     = l;
+	}
+
+	public function getEnvScore() {
+		return envScore;
+	}
+
+	public function setEnvScore( addend : int ) {
+		var tmpEnvScore : int;
+		tmpEnvScore = getEnvScore() + addend;
+		if (tmpEnvScore > 100) tmpEnvScore = 100;
+		if (tmpEnvScore < 0) tmpEnvScore = 0;
+		envScore = tmpEnvScore;
+	}
+
+	public function getHappiness() {
+		return happiness;
+	};
+
+	public function setHappiness( addend : int ) {
+		var tmpHappiness : int;
+		tmpHappiness = happiness + addend;
+		if (tmpHappiness > 100) tmpHappiness = 100;
+		if (tmpHappiness < 0) tmpHappiness = 0;
+		happiness = tmpHappiness;
+	};
+
+	// State update functions
+	public function settle() {};
+	public function unsettle() {};
+	public function hold() {};
 }
 
-function Update () {
+var cat : Cat;
+cat = new Cat( 50, 50, {
+	"hot" : 0.8,
+	"wet" : -1
+});
 
+function Start() {}
+function Update() {}
+
+function OnTriggerEnter( col : Collider ) {
+	var zone : Zone;
+	var heat : int;
+	heat = col.gameObject.GetComponent.<Zone>().hot;
+	// score the mood of the cat
+	var score : int = cat.getEnvScore();
+	//score += heat * cat.likes['hot'];
+	cat.settle();
 }
